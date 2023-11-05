d89a19f03fe7e4b705c28116efc95c247f434778
parmarraj999 <rajraja5122@gmail.com>
2023-11-05 01:12:49 -0800

    landing done

--- src/layouts/landingPage/landing.js
+++ src/layouts/landingPage/landing.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import "./landing.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 function Landing() {
 
@@ -16,7 +18,22 @@ function Landing() {
         <div className={theme ? "toggle-container bb fs" : "toggle-container bw fe"} onClick={()=>setTheme(!theme)}>
           <div className={theme ? "toggle-button bw" : "toggle-button bb"}></div>
         </div>
-        <h1 style={ theme ? {color:"black"} : {color:"white"}}>A new <span style={{background:"rgb(32, 15, 49)",color:"rgb(179, 102, 255)"}}>way</span> to <span style={{background:"rgb(14, 50, 20)",color:"rgb(0, 255, 38)"}}>enjoy</span></h1>
+        <div>
+          <h1>Logo</h1>
+        </div>
+        <div>
+          <h1 style={ theme ? {color:"black"} : {color:"white"}}>A new <span style={{background:"rgb(32, 15, 49)",color:"rgb(179, 102, 255)"}}>way</span> to <span style={{background:"rgb(14, 50, 20)",color:"rgb(0, 255, 38)"}}>enjoy</span></h1>
+          <p className='para'>This is the plateform where you arrange your party with perfect analysis and systematic way</p>
+        </div>
+        <div className="btn-container" >
+          <div className={theme ? "organize-btn bb":"organize-btn bw"}>
+            <a href=''>Organize Party</a>
+            <div>
+            <FontAwesomeIcon icon={faArrowRight} style={theme ? {color:"white"} : {color:"black"}}/>
+            </div>
+          </div>
+          <button >Join Party</button>
+        </div>
       </div>
     </div>
   )
