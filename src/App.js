d08fca95cfba192cc89b389f8222728aa53cfb63
parmarraj999 <rajraja5122@gmail.com>
2023-11-06 13:31:33 -0800

    user and admin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { BrowserRouter,Routes ,Route } from 'react-router-dom'
 import Landing from './layouts/landingPage/landing'
+import Dashboard from './layouts/admin/dashboard'
+import User from './layouts/user/user'
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Landing/>}/> 
+          <Route path='/admin' element={<Dashboard/>}/>
+          <Route path='/user' element={<User/>}/>
         </Routes>
       </BrowserRouter>
     </div>
--- src/layouts/landingPage/landing.js
+++ src/layouts/landingPage/landing.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 
 function Landing() {
 
-  const [theme,setTheme] = useState(true);
+  const [theme, setTheme] = useState(true);
 
   return (
     <div className={theme ? "landing-wrapper bw" : "landing-wrapper bb"}>
@@ -16,24 +16,24 @@ function Landing() {
         <section id="third"></section>
       </div>
       <div className='landing-page-container'>
-        <div className={theme ? "toggle-container bb fs" : "toggle-container bw fe"} onClick={()=>setTheme(!theme)}>
+        <div className={theme ? "toggle-container bb fs" : "toggle-container bw fe"} onClick={() => setTheme(!theme)}>
           <div className={theme ? "toggle-button bw" : "toggle-button bb"}></div>
         </div>
         <div>
           <h1>Logo</h1>
         </div>
-        <div style={{width:"90%",display:"flex",flexDirection:"column",alignItems:"center"}}>
-          <h1 style={ theme ? {color:"black"} : {color:"white"}}>A new <span style={{background:"rgb(32, 15, 49)",color:"rgb(179, 102, 255)"}}>way</span> to <span style={{background:"rgb(14, 50, 20)",color:"rgb(0, 255, 38)"}}>enjoy</span></h1>
+        <div style={{ width: "90%", display: "flex", flexDirection: "column", alignItems: "center" }}>
+          <h1 style={theme ? { color: "black" } : { color: "white" }}>A new <span style={{ background: "rgb(32, 15, 49)", color: "rgb(179, 102, 255)" }}>way</span> to <span style={{ background: "rgb(14, 50, 20)", color: "rgb(0, 255, 38)" }}>enjoy</span></h1>
           <p className='para'>This is the plateform where you arrange your party with perfect analysis and systematic way</p>
         </div>
         <div className="btn-container" >
-          <div className={theme ? "organize-btn bb":"organize-btn bw"}>
-            <Link className='Obtn' to="">Organize Party</Link>
+          <Link to="/admin" className={theme ? "organize-btn bb" : "organize-btn bw"}>
+            <div className='Obtn' to="">Organize Party</div>
             <div>
-            <FontAwesomeIcon icon={faArrowRight} style={theme ? {color:"white"} : {color:"black"}}/>
+              <FontAwesomeIcon icon={faArrowRight} style={theme ? { color: "white" } : { color: "black" }} />
             </div>
-          </div>
-          <button >Join Party</button>
+          </Link>
+            <Link className='button' to="/user" >Join Party</Link>
         </div>
       </div>
     </div>
